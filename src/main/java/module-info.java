cec91aba58e6f47022deb156505031b8db239031
Alex111145 <alessio.gervasini45@gmail.com>
2025-05-06 16:41:11 +0200

    fa il server in locale e non chiude il client

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module book_recommender.lab_b {
     requires java.desktop;
     requires java.net.http;
     requires javafx.graphics;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
 
     opens book_recommender.lab_b to javafx.fxml;
     exports book_recommender.lab_b;
